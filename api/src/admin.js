a32a8c91996a6434e7f8b410897f34e908b607ab
Moinul Islam <moinulilm10@gmail.com>
2025-07-09 11:38:23 +0600

    feat: enhance admin.js by improving comments and structuring resources for clarity

--- api/src/admin.js
+++ api/src/admin.js
@@ -1,18 +1,24 @@
-// admin.js
 import AdminJSExpress from "@adminjs/express";
 import * as AdminJSMongoose from "@adminjs/mongoose";
 import AdminJS from "adminjs";
 import { Product } from "./models/Product.js";
 
-// Register adapter
+// Register the Mongoose adapter
 AdminJS.registerAdapter({
   Resource: AdminJSMongoose.Resource,
   Database: AdminJSMongoose.Database,
 });
 
-// Add your models when ready
 const adminJs = new AdminJS({
-  resources: [{ resource: Product }],
+  resources: [
+    {
+      resource: Product,
+      options: {
+        navigation: null,
+        name: "Product",
+      },
+    },
+  ],
   rootPath: "/admin",
 });
 
